07162e47f87672765a75acecd4461d439b2b36b3
Anas-Faiz <anasfaiz0811@gmail.com>
2026-01-02 22:02:18 -0800

    server connected to database

--- project_2/auth_backend/app.js
+++ project_2/auth_backend/app.js
@@ -1,4 +1,28 @@
 const express= require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { connectDB } = require("./config/database");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json);
+
+const port = 4000;
+
+connectDB()
+.then(()=>{
+     console.log("database connected")
+    app.listen(port,()=>{
+      console.log("server running on " + port);
+    })  
+}).catch((error)=>{
+  console.log("error connecting the database " + error)
+})
+
+
+
+
+
+
 
---
+++ project_2/auth_backend/config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+async function connectDB (){
+    await mongoose.connect(process.env.mongoose_url)
+    
+}
+
+module.exports =  {connectDB };
