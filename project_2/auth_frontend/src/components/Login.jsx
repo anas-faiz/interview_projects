3caf741131303a3af3184b533a70ba246b862b97
Anas-Faiz <anasfaiz0811@gmail.com>
2026-01-03 23:02:29 -0800

    login page

--- project_2/auth_frontend/src/components/Login.jsx
+++ project_2/auth_frontend/src/components/Login.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
-  return (
-    <div>Login</div>
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    
+    const handleClick = ()=>{
+        
+    }
+
+    return (
+    <div>
+        <input value={email} onChange={(e)=>setEmail(e.target.value)} />
+        <input value={password} onChange={(e)=>setPassword(e.target.value)} />
+        <button onClick={handleClick} >click</button>
+    </div>
   )
 }
 
