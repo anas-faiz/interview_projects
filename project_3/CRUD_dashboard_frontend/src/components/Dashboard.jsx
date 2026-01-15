8478a909a64a64e5f66caf2d7f125b7dfa5b664d
Anas-Faiz <anasfaiz0811@gmail.com>
2026-01-14 19:52:56 -0800

    add product endpoint

--- project_3/CRUD_dashboard_frontend/src/components/Dashboard.jsx
+++ project_3/CRUD_dashboard_frontend/src/components/Dashboard.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react"
+import axios from "axios"
 
 const Dashboard = ()=>{
     const [ productName, setProductName ] = useState("");
     const [price,setPrice] = useState('');
     const [stock,setStock] = useState('');
     const [category,setCategory] = useState('');
+
+    const handleSubmit = async ()=>{
+        const res = await axios.post("http:localhost:3000/product/add",{
+            productName,
+            price,
+            stock,
+            category,
+        })
+
+        const data = await res.json()
+    }
 
     return(
         <div>
@@ -14,6 +26,7 @@ const Dashboard = ()=>{
                 <input value={price} onChange={(e)=>setPrice(e.target.value)}  placeholder="price" />
                 <input value={stock} onChange={(e)=>setStock(e.target.value)} placeholder="stock" />
                 <input value={category} onChange={(e)=>setCategory(e.target.value)} placeholder="category" />                
+                <button onSubmit={handleSubmit} >ADD product</button>
             </form>
         </div>
     )
